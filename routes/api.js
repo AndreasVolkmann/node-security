40976a4e6f525b6d777e2745b42c61c574915518
goreRatzete <avolkmann@icloud.com>
2016-04-09 11:59:30 +0200

    set up for db

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/names', function (req, res, next) {
+    res.json([{name: 'Peter'}, {name: 'Kurt'}, {name: 'Hanne'}]);
+});
+
+router.get('/hellos', function (req, res) {
+    res.json([{msg: 'Hello World'}, {msg: 'こんにちは'}, {msg: 'Hello Guys!'}]);
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('./config/database');
+var User = require('./app/models/user');
+var jwt = require('jwt-simple');
 
 var routes = require('./routes/index');
+var restApi = require('./routes/api');
 var users = require('./routes/users');
 
 var app = express();
@@ -21,8 +27,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
+app.use('/api', restApi);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
