09f82a68ed945e10d16506645b3574000938ef84
av <avolkmann@icloud.com>
2016-05-15 16:38:15 +0200

    server.js spelling errors

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use('/', routes);
 app.use('/api', function (req, res, next) {
     passport.authenticate('jwt', {session: false}, function (err, user, info) {
         if (err) {
-            res.status(403).json({mesage: "Token could not be authenticated", fullError: err})
+            res.status(403).json({message: "Token could not be authenticated", fullError: err})
         }
         if (user) {
             return next();
         }
-        return res.status(403).json({mesage: "Token could not be authenticated", fullError: info});
+        return res.status(403).json({message: "Token could not be authenticated", fullError: info});
     })(req, res, next);
 });
 app.use('/api', restApi);
